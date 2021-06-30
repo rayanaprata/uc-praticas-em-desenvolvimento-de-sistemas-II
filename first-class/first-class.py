3060d4d06a6ff9f155fe5075361ba4ae81e3039a
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-06-30 20:34:22 -0300

    Initial commit

---
+++ first-class/first-class.py
@@ -0,0 +1,15 @@
+
+"""  Implementar um programa em Python que recebe os
+dados de nome, idade, altura, peso e calcule o IMC.  """
+
+name = input("Type your name: ")
+age = input("Type your age: ")
+height = float(input("Type your height: "))
+weight = float(input("Type your weight: "))
+
+imc = weight / (height*2)
+
+print(name, "have", age, "years old and,", weight, "kilos and measures", height, "meters. Your IMC is", imc)
+
+# print(f '{name} have {age} years old and {weight} kilos and measures {height} meters. Your IMC is {imc}.')
+

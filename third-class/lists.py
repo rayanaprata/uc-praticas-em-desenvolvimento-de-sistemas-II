3e9ca6c522309a04c74338efbe4461fc3e65d100
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-07-05 21:02:22 -0300

    listas em python

---
+++ third-class/lists.py
@@ -0,0 +1,14 @@
+
+# create a list with 15 elements
+
+for v in range(15):
+  print(v)
+
+# creating a list with ranges
+
+l = list(range(100, 1100, 50))
+print(l)
+
+for t in range(3, 33, 3):
+  print(t, end=" - ")
+print()
